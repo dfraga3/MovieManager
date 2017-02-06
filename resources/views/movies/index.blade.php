e89d5f3e0dbbd21f4105bd6905845263f7a9ba42
dfraga2 <dfraga@job.com>
2017-02-06 15:51:45 -0500

    Index page having an issue on Heroku.  Testing cause.

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -12,16 +12,6 @@
         <div class="col-xs-2 col-sm-2 col-md-2 panel-title"><strong>Rating</strong></div>
     </div>
 
-    @foreach($movies as $movie)
-        <div class="row">
-            <div class="col-xs-12 col-sm-3 col-md-3"><strong><a href="{{ route('movies.edit', [$movie->id]) }}">{{ $movie->title  }}</a></strong></div>
-            <div class="col-xs-3 col-sm-1 col-md-1">{{ $movie->year }}</div>
-            <div class="col-xs-2 col-sm-2 col-md-2">{{ convertToHoursMins($movie->length, '%2d hr %02d m') }}</div>
-            <div class="col-xs-3 col-sm-2 col-md-2">{{ displayFormat($movie->format) }}</div>
-            <div class="col-xs-2 col-sm-2 col-md-2"><span class="rating-static rating-{{ $movie->rating }}0"></span></div>
-        </div>
-    @endforeach
-
 </div>
 @endsection
 
