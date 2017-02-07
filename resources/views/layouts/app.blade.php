12081a9dbf683ab4f3835ff92b746ccd8e3b4c57
dfraga2 <dfraga@job.com>
2017-02-07 10:57:34 -0500

    Implemented sticky footer and put the collapsable nav bar back in (accidentally removed with prior commit).

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,10 @@
     <title>{{ config('app.name', '') }}</title>
 
     <!-- Styles -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:100,600" type="text/css" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" />
-    <link href="/css/app.css" rel="stylesheet" />
-    <link rel="stylesheet" href="{{ URL::asset('/css/stars.css') }}" />
+    <link rel="stylesheet" href="/css/app.css"  />
+    <link rel="stylesheet" href="/css/stars.css" />
 
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
@@ -25,97 +25,102 @@
     </script>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">
-
-
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', '') }}</a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        <li><a href="{{ url('/') }}">Home</a></li>
-                        <li><a href="{{ url('/movies') }}">Movies</a></li>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">Login</a></li>
-                            <li><a href="{{ url('/register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
+<div id="app">
+    <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
-            <div class="row">
-                <div class="col-md-10 col-md-offset-2">
-                    <div class="panel panel-default">
-                        @if (Session::has('flash_message'))
-                            <div class="alert alert-success">{{ Session::get('flash_message') }}</div>
-                        @endif
+            <div class="navbar-header">
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
 
-                        @yield('content')
-                    </div>
-                    <div class="panel-footer" id="weather"></div>
-                </div>
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', '') }}</a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/') }}">Home</a></li>
+                    <li><a href="{{ url('/movies') }}">Movies</a></li>
+                </ul>
+
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
             </div>
         </div>
+    </nav>
 
-        <footer class="footer">
-            <div class="container">
-                <p class="text-muted credit">
-            <span style="text-align: left; float: left">
-                <img src="{{ URL::asset('/img/FS_Cans.png') }}" height="40" width="40" />
-                &copy; 2017
-            </span>
-                    <span class="hidden-phone" style="text-align: right; float: right">Powered by: Cherry-Coke Zero</span>
-                </p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-2">
+                <div class="panel panel-default">
+                    @if (Session::has('flash_message'))
+                        <div class="alert alert-success">{{ Session::get('flash_message') }}</div>
+                    @endif
+
+                    @yield('content')
+                </div>
+                <div class="panel-footer" id="weather"></div>
             </div>
-        </footer>
+        </div>
     </div>
 
-    <!-- Scripts -->
-    <script src="/js/app.js"></script>
-    <script>
-        jQuery(document).ready(function($) {
-            $.ajax({
-                url : "http://api.wunderground.com/api/aeeff752dec45a70/geolookup/conditions/q/VA/Richmond.json",
-                dataType : "jsonp",
-                success : function(parsed_json) {
-                    var location = parsed_json['location']['city'];
-                    var temp_f = parsed_json['current_observation']['temp_f'];
-                    $("#weather").html("Current temperature in " + location + " is: " + temp_f);
-                }
-            });
+    <footer class="footer">
+        <div class="container">
+            <p class="text-muted credit">
+                <span style="text-align: left; float: left">
+                    <img src="{{ URL::asset('/img/FS_Cans.png') }}" height="40" width="40" />
+                    &copy; 2017
+                </span>
+                <span class="hidden-phone" style="text-align: right; float: right">Powered by: Cherry-Coke Zero</span>
+            </p>
+        </div>
+    </footer>
+</div>
+
+<!-- Scripts -->
+<script src="/js/app.js"></script>
+<script>
+    jQuery(document).ready(function($) {
+        $.ajax({
+            url : "http://api.wunderground.com/api/aeeff752dec45a70/geolookup/conditions/q/VA/Richmond.json",
+            dataType : "jsonp",
+            success : function(parsed_json) {
+                var location = parsed_json['location']['city'];
+                var temp_f = parsed_json['current_observation']['temp_f'];
+                $("#weather").html("Current temperature in " + location + " is: " + temp_f);
+            }
         });
-    </script>
-    @yield('jQuery')
+    });
+</script>
+@yield('jQuery')
 </body>
 </html>
